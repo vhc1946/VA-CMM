b746aed3d7e41b94b0e9f3e390263de34cf571cc
Ryan Murphy <ryanm@vogelheating.com>
2022-10-27 13:36:56 -0500

    styling to address block

--- bin/gui/client-form.js
+++ bin/gui/client-form.js
@@ -1,7 +1,9 @@
+var gentools = require("../repo/tools/box/vg-gentools.js")
+
 var CREATEprojectforms=(curragree)=>{   // Sets up layout using ViewControl
-    let block = NEWvcsetup();
+    let block = document.createElement('div');
     block.id = 'build-info-edit';
-    vcontrol.SETUPviews(block,'mt');
+    vcontrol.CREATEviewport(block,'mt');
     
     cont = document.createElement('div');
     cont.id = 'CLIENT';
@@ -16,19 +18,6 @@ var CREATEprojectforms=(curragree)=>{   // Sets up layout using ViewControl
     cont.appendChild(CREATEinputform(curragree,'project',projectinclude,true));
 
     return block;
-}
-
-
-var NEWvcsetup=()=>{
-    let vcblock = document.createElement('div');  // Creates new ViewControl setup!
-    vcblock.classList.add('viewcontrol-cont');
-    vcblock.appendChild(document.createElement('div'));
-    vcblock.lastChild.classList.add('viewcontrol-menu');
-    vcblock.lastChild.appendChild(document.createElement('div'));
-    vcblock.appendChild(document.createElement('div'));
-    vcblock.lastChild.classList.add('viewcontrol-port');
-  
-    return vcblock;
 }
 
 var CREATEinputform=(data,type,compare=[],include=false)=>{   // Generic function to create input form based on data's structure
@@ -46,55 +35,14 @@ var CREATEinputform=(data,type,compare=[],include=false)=>{   // Generic functio
 
 var CREATEinputfield=(cont,eg,type)=>{
     cont.appendChild(document.createElement('label'));
-    cont.lastChild.innerText = toTitleCase(eg);
+    cont.lastChild.classList.add(`${type}-label-${eg}`);
+    cont.lastChild.innerText = gentools.toTitleCase(eg);
     cont.appendChild(document.createElement('input'));
-    cont.lastChild.classList.add(`${type}-${eg}`);
+    cont.lastChild.classList.add(`${type}-input-${eg}`);
 }
 
 
-var CREATEjobform=(curragree)=>{
-    let ins = document.createElement('div');
-    ins.classList.add('project-info-cont');
 
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "Project Name";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-name`);
-    
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "Street";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-street`);
-
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "Unit";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-unit`);
-    
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "City";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-city`);
-    
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "State";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-state`);
-
-    ins.appendChild(document.createElement('label'));
-    ins.lastChild.innerText = "Zip";
-    ins.appendChild(document.createElement('input'));
-    ins.lastChild.classList.add(`project-zip`);
-
-    return ins;
-}
-
-function toTitleCase(str) {
-    var lcStr = str.toLowerCase();
-    return lcStr.replace(/(?:^|\s)\w/g, function(match) {
-    return match.toUpperCase();
-    });
-}
 
 module.exports={
     CREATEprojectforms
